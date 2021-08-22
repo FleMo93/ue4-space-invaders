9de89b442741ec9c344efaf1a3a980a2890a765f
FleMo93 <florianhammer@hotmail.de>
2021-08-22 08:46:55 +0200

    Enemy shooting

--- Source/SpaveInvaders/EnemyBlock.cpp
+++ Source/SpaveInvaders/EnemyBlock.cpp
@@ -4,150 +4,152 @@
 
 AEnemyBlock::AEnemyBlock()
 {
-	PrimaryActorTick.bCanEverTick = true;
+  PrimaryActorTick.bCanEverTick = true;
 }
 
 void AEnemyBlock::BeginPlay()
 {
-	Super::BeginPlay();
-	MyWorld = GetWorld();
-	CurrentMovementDirection = InitialMovementDirection;
-	Alive = true;
-	CharacterMovementComponent = Cast<UCharacterMovementComponent>(GetComponentByClass(UCharacterMovementComponent::StaticClass()));
-	CharacterMovementComponent->GravityScale = 0;
-	CharacterMovementComponent->MovementMode = EMovementMode::MOVE_Flying;
+  Super::BeginPlay();
+  MyWorld = GetWorld();
+  CurrentMovementDirection = InitialMovementDirection;
+  IsAlive = true;
+  CharacterMovementComponent = Cast<UCharacterMovementComponent>(GetComponentByClass(UCharacterMovementComponent::StaticClass()));
+  CharacterMovementComponent->GravityScale = 0;
+  CharacterMovementComponent->MovementMode = EMovementMode::MOVE_Flying;
 }
 
 void AEnemyBlock::MoveBlock_Implementation()
 {
-	FVector Origin, Extend;
-	GetActorBounds(true, Origin, Extend, true);
-	auto Location = GetActorLocation();
-	auto BoundsOffset = FVector(
-		Location.X - Origin.X + Extend.X,
-		Location.Y - Origin.Y + Extend.Y,
-		Location.Z - Origin.Z + Extend.Z);
+  FVector Origin, Extend;
+  GetActorBounds(true, Origin, Extend, true);
+  auto Location = GetActorLocation();
+  auto BoundsOffset = FVector(
+    Location.X - Origin.X + Extend.X,
+    Location.Y - Origin.Y + Extend.Y,
+    Location.Z - Origin.Z + Extend.Z);
 
-	auto BoundHit = false;
-	FVector NewLocation;
+  auto BoundHit = false;
+  FVector NewLocation;
 
-	if (CurrentMovementDirection == Left)
-	{
-		float LeftPosition = Origin.X - Extend.X;
-		if (LeftPosition <= LeftBound)
-		{
-			CurrentMovementDirection = MovementDirection::Right;
-			NewLocation = FVector(LeftPosition - BoundsOffset.X, Location.Y, Location.Z - RowHeightMovement);
-			BoundHit = true;
-		}
-	}
-	else if (CurrentMovementDirection == Right)
-	{
-		float RightPosition = Origin.X + Extend.X;
-		if (RightPosition >= RightBound)
-		{
-			CurrentMovementDirection = MovementDirection::Left;
-			NewLocation = FVector(RightPosition - (2 * Extend.X), Location.Y, Location.Z - RowHeightMovement);
-			BoundHit = true;
-		}
-	}
+  if (CurrentMovementDirection == Left)
+  {
+    float LeftPosition = Origin.X - Extend.X;
+    if (LeftPosition <= LeftBound)
+    {
+      CurrentMovementDirection = MovementDirection::Right;
+      NewLocation = FVector(LeftPosition - BoundsOffset.X, Location.Y, Location.Z - RowHeightMovement);
+      BoundHit = true;
+    }
+  }
+  else if (CurrentMovementDirection == Right)
+  {
+    float RightPosition = Origin.X + Extend.X;
+    if (RightPosition >= RightBound)
+    {
+      CurrentMovementDirection = MovementDirection::Left;
+      NewLocation = FVector(RightPosition - (2 * Extend.X), Location.Y, Location.Z - RowHeightMovement);
+      BoundHit = true;
+    }
+  }
 
-	if (BoundHit)
-	{
-		CharacterMovementComponent->StopMovementImmediately();
-		SetActorLocation(NewLocation, false, nullptr, ETeleportType::None);
-		CharacterMovementComponent->MaxFlySpeed += MovementSpeedIncrease;
-	}
+  if (BoundHit)
+  {
+    CharacterMovementComponent->StopMovementImmediately();
+    SetActorLocation(NewLocation, false, nullptr, ETeleportType::None);
+    CharacterMovementComponent->MaxFlySpeed += MovementSpeedIncrease;
+  }
 
-	float Movement = (CurrentMovementDirection == Left) ? -1 : 1;
-	AddMovementInput(FVector(Movement, 0, 0));
+  float Movement = (CurrentMovementDirection == Left) ? -1 : 1;
+  AddMovementInput(FVector(Movement, 0, 0));
 }
 
 void AEnemyBlock::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-	if (GetNetMode() == ENetMode::NM_Client)
-		return;
+  Super::Tick(DeltaTime);
+  if (GetNetMode() == ENetMode::NM_Client)
+    return;
 
-	if (!Alive || MyWorld->IsPaused())
-		return;
+  if (!IsAlive || MyWorld->IsPaused())
+    return;
 
-	if (GetNetMode() != ENetMode::NM_Client)
-		MoveBlock_Implementation();
+  if (GetNetMode() != ENetMode::NM_Client)
+    MoveBlock_Implementation();
 }
 
 ABaseEnemy* AEnemyBlock::SpawnEnemyChild(TSubclassOf<ABaseEnemy> EnemyClass)
 {
-	auto EnemyActor = GetWorld()->SpawnActor(EnemyClass);
-	auto AttRules = FAttachmentTransformRules(EAttachmentRule::KeepRelative, false);
-	EnemyActor->AttachToActor(this, AttRules);
-	return Cast<ABaseEnemy>(EnemyActor);
+  auto EnemyActor = GetWorld()->SpawnActor(EnemyClass);
+  auto AttRules = FAttachmentTransformRules(EAttachmentRule::KeepRelative, false);
+  EnemyActor->AttachToActor(this, AttRules);
+  return Cast<ABaseEnemy>(EnemyActor);
+}
+
+void AEnemyBlock::CheckIfIsAlive()
+{
+  IsAlive = EnemyColumns.Num() > 0;
 }
 
 void AEnemyBlock::OnEnemyDestroyed(AActor* Act)
 {
-	for (auto Column : this->EnemyColumns)
-	{
-		for (auto Enemy : Column.Enemies)
-		{
-			if (Enemy == Act)
-			{
-				Column.Enemies.RemoveSwap(Enemy);
-				Enemy->OnDestroyed.RemoveAll(this);
-				auto Length = Column.Enemies.Num();
+  for (auto X = 0; X < EnemyColumns.Num(); X++)
+    for (auto Y = 0; Y < EnemyColumns[X].Enemies.Num(); Y++) {
+      if (EnemyColumns[X].Enemies[Y] != Act)
+        continue;
 
-				/*if (Length < 0)
-				{
-					this->EnemyColumns.RemoveSwap(Column);
-				}*/
+      EnemyColumns[X].Enemies.RemoveAt(Y, 1, true);
 
-				/*if (Column.Enemies.Num() == 0)
-					this->EnemyColumns.RemoveSwap(Column);*/
-				return;
-			}
-		}
-	}
+      if (EnemyColumns[X].Enemies.Num() == 0) {
+        EnemyColumns.RemoveAt(X, 1, true);
+        CheckIfIsAlive();
+      }
+
+      return;
+    }
 }
 
 TArray<FEnemyColumn> AEnemyBlock::SpawnEnemies(int Columns, int Rows, float SpawnOffset, TSubclassOf<ABaseEnemy> EnemyClass)
 {
-	for (auto Column : this->EnemyColumns)
-		for (auto Enemy : Column.Enemies)
-			Enemy->Destroy();
+  for (auto Column : this->EnemyColumns)
+    for (auto Enemy : Column.Enemies)
+      Enemy->Destroy();
 
-	this->EnemyColumns = {};
+  this->EnemyColumns = {};
 
-	for (int X = 0; X < Columns; X++)
-	{
-		FEnemyColumn EnemyColumn = FEnemyColumn();
+  for (int X = 0; X < Columns; X++)
+  {
+    FEnemyColumn EnemyColumn = FEnemyColumn();
 
-		for (int Z = 0; Z < Rows; Z++)
-		{
-			auto Enemy = SpawnEnemyChild(EnemyClass);
-			FScriptDelegate Delegate;
-			Delegate.BindUFunction(this, "OnEnemyDestroyed");
-			Enemy->OnDestroyed.AddUnique(Delegate);
+    for (int Z = 0; Z < Rows; Z++)
+    {
+      auto Enemy = SpawnEnemyChild(EnemyClass);
+      FScriptDelegate Delegate;
+      Delegate.BindUFunction(this, "OnEnemyDestroyed");
+      Enemy->OnDestroyed.AddUnique(Delegate);
 
-			EnemyColumn.Enemies.Add(Enemy);
-			FVector ActorOrigin = FVector();
-			FVector ActorBoxExtend = FVector();
-			Enemy->GetActorBounds(false, ActorOrigin, ActorBoxExtend);
-			ActorOrigin.X = SpawnOffset * X;
-			ActorOrigin.Y = 0;
-			ActorOrigin.Z = SpawnOffset * -1 * Z;
-			Enemy->SetActorRelativeLocation(ActorOrigin);
-		}
+      EnemyColumn.Enemies.Add(Enemy);
+      FVector ActorOrigin = FVector();
+      FVector ActorBoxExtend = FVector();
+      Enemy->GetActorBounds(false, ActorOrigin, ActorBoxExtend);
+      ActorOrigin.X = SpawnOffset * X;
+      ActorOrigin.Y = 0;
+      ActorOrigin.Z = SpawnOffset * -1 * Z;
+      Enemy->SetActorRelativeLocation(ActorOrigin);
+    }
 
-		EnemyColumns.Add(EnemyColumn);
-	}
+    EnemyColumns.Add(EnemyColumn);
+  }
 
-	//this->EnemyColumns = EnemyColumns;
-	return EnemyColumns;
+  CheckIfIsAlive();
+
+  return EnemyColumns;
 }
 
-ABaseEnemy* AEnemyBlock::GetBottomAnyEnemy()
+ABaseEnemy* AEnemyBlock::GetAnyBottomEnemy()
 {
-	//auto Length = this->EnemyColumns.Num();
-	//RandRange(0, Length);
-	return NULL;
+  if (!IsAlive)
+    return NULL;
+
+  auto ColumnIndex = FMath::RandRange(0, EnemyColumns.Num() - 1);
+  auto RowIndex = FMath::RandRange(0, EnemyColumns.Num() - 1);
+  return EnemyColumns[ColumnIndex].Enemies[RowIndex];
 }
--- Source/SpaveInvaders/EnemyBlock.h
+++ Source/SpaveInvaders/EnemyBlock.h
@@ -16,69 +16,70 @@
 USTRUCT(BlueprintType)
 struct FEnemyColumn
 {
-	GENERATED_BODY()
+  GENERATED_BODY()
 
-		UPROPERTY(BlueprintReadOnly)
-		TArray<ABaseEnemy*> Enemies;
+    UPROPERTY(BlueprintReadOnly)
+    TArray<ABaseEnemy*> Enemies;
 };
 
 UENUM(BlueprintType)
 enum MovementDirection
 {
-	Left = 0 UMETA(DisplayName = "Left"),
-	Right = 1 UMETA(DUsplaceName = "Right"),
+  Left = 0 UMETA(DisplayName = "Left"),
+  Right = 1 UMETA(DUsplaceName = "Right"),
 };
 
 UCLASS()
 class SPAVEINVADERS_API AEnemyBlock : public ACharacter
 {
-	GENERATED_BODY()
+  GENERATED_BODY()
 
 private:
-	bool Alive = false;
-	UWorld* MyWorld;
-	TEnumAsByte<MovementDirection> CurrentMovementDirection;
-	UCharacterMovementComponent* CharacterMovementComponent;
+  bool IsAlive = false;
+  UWorld* MyWorld;
+  TEnumAsByte<MovementDirection> CurrentMovementDirection;
+  UCharacterMovementComponent* CharacterMovementComponent;
 
 public:
-	AEnemyBlock();
+  AEnemyBlock();
 
-	UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Behaviour")
-		float LeftBound = 0;
+  UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Behaviour")
+    float LeftBound = 0;
 
-	UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Behaviour")
-		float RightBound = 100;
+  UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Behaviour")
+    float RightBound = 100;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Behaviour")
-		float MovementSpeedIncrease = 50;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Behaviour")
+    float MovementSpeedIncrease = 50;
 
-	UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Behaviour")
-		TEnumAsByte<MovementDirection> InitialMovementDirection;
+  UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Behaviour")
+    TEnumAsByte<MovementDirection> InitialMovementDirection;
 
-	UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Behaviour")
-		float RowHeightMovement = 100;
+  UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Behaviour")
+    float RowHeightMovement = 100;
 
-	UPROPERTY(EditAnyWhere, BlueprintReadOnly)
-		TArray<FEnemyColumn> EnemyColumns = {};
+  UPROPERTY(EditAnyWhere, BlueprintReadOnly)
+    TArray<FEnemyColumn> EnemyColumns = {};
 
 private:
-	UFUNCTION(Server, Unreliable)
-		void MoveBlock();
+  UFUNCTION(Server, Unreliable)
+    void MoveBlock();
 
-	UFUNCTION()
-		void OnEnemyDestroyed(AActor* Act);
+  UFUNCTION()
+    void OnEnemyDestroyed(AActor* Act);
 
-	ABaseEnemy* SpawnEnemyChild(TSubclassOf<ABaseEnemy> EnemyClass);
+  ABaseEnemy* SpawnEnemyChild(TSubclassOf<ABaseEnemy> EnemyClass);
+  void CheckIfIsAlive();
 
 protected:
-	virtual void BeginPlay() override;
+  virtual void BeginPlay() override;
 
 public:
-	virtual void Tick(float DeltaTime) override;
+  virtual void Tick(float DeltaTime) override;
 
-	UFUNCTION(BlueprintCallable)
-		TArray<FEnemyColumn> SpawnEnemies(int Columns, int Rows, float SpawnOffset, TSubclassOf<ABaseEnemy> EnemyClass);
+  UFUNCTION(BlueprintCallable)
+    TArray<FEnemyColumn> SpawnEnemies(int Columns, int Rows, float SpawnOffset, TSubclassOf<ABaseEnemy> EnemyClass);
 
-	UFUNCTION(BlueprintCallable)
-		ABaseEnemy* GetBottomAnyEnemy();
+  UFUNCTION(BlueprintCallable)
+    ABaseEnemy* GetAnyBottomEnemy();
 };
